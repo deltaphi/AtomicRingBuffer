567fedab52ce9ea409b4dbd7eb62d3fc17782dd2
Damian Philipp <damian.philipp@gmx.net>
2020-12-18 16:55:49 +0100

    Beautifying

--- AtomicRingBuffer/StringCopyHelper.cpp
+++ AtomicRingBuffer/StringCopyHelper.cpp
@@ -3,34 +3,18 @@
 namespace AtomicRingBuffer {
 size_t memcpyCharReplace(char*& dest, const char* src, char search, const char* replace, size_t destLen,
                          size_t srcLen) {
+  // Check preconditions
   if (src == nullptr || dest == nullptr || srcLen == 0 || destLen == 0) {
     // Nothing to do.
     return 0;
   }
 
-  /*
-    // Count number of newlines
-  const char* lastNewLine = src;
-  const char* nextNewLine = src;
-  std::size_t numSearchFinds = 0;
-  std::size_t bytesAnalyzed = 0;
-  while (nextNewLine != nullptr) {
-    nextNewLine = static_cast<const char*>(memchr(nextNewLine, search, srcLen - bytesAnalyzed));
-    if (nextNewLine != nullptr) {
-      ++numSearchFinds;
-      bytesAnalyzed += nextNewLine - lastNewLine;
-      lastNewLine = nextNewLine;
-      ++nextNewLine;
-    }
-  }
-  */
-
   std::size_t readIdx = 0;
   std::size_t writeIdx = 0;
 
+  // Figure out how long the replacement string actually is.
   const std::size_t replaceLen = (replace != nullptr ? strlen(replace) : 0);
 
-  // TODO: If ptr[readIdx] == '\n' we need two bytes to be available!
   while (writeIdx < destLen && readIdx < srcLen) {
     if (src[readIdx] == search) {
       if (writeIdx >= destLen - replaceLen + 1) {
@@ -38,7 +22,7 @@ size_t memcpyCharReplace(char*& dest, const char* src, char search, const char* 
         // Break the loop if it won't fit.
         break;
       } else {
-        // Does fit. Now copy.
+        // Does fit. Now copy the replacement string.
         memcpy(&dest[writeIdx], replace, replaceLen);
         writeIdx += replaceLen;
       }
@@ -49,6 +33,7 @@ size_t memcpyCharReplace(char*& dest, const char* src, char search, const char* 
     ++readIdx;
   }
 
+  // Setup return values
   dest = &dest[writeIdx];
   return readIdx;
 }
